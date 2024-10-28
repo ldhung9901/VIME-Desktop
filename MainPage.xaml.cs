4da8b8abac3d7a4550793a6230a1dc9a33b8a3f1
hung.dev <hung.dev@fake.cm>
2024-10-29 01:10:30 +0700

    feat: new scan reader

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,67 +1,118 @@
 ﻿using Microsoft.JSInterop;
+using PCSC;
+using PCSC.Monitoring;
 using RAR.IdCard.Sdk.Reader;
 
 namespace VimeDesktopApp;
 
 public partial class MainPage : ContentPage
 {
-    // private static VnIdcard _cardReader = new VnIdcard();
+    private static readonly VnIdcard CardReader = new VnIdcard();
+    private static CardInfo _cardInfo;
     public MainPage()
     {
+        _cardInfo = new CardInfo();
         InitializeComponent();
-        // TryScanCard();
+        CardReader.OnStatusChanged += CardReader_OnStatusChanged;
+        CardReader.StartMonitor(false);
     }
     [JSInvokable]
-    public static string CheckBattery()
+    public static CardInfo CheckBattery()
     {
-        string batteryDetails = "This device," + DeviceInfo.Name + ", has battery level of " + Math.Floor(Battery.ChargeLevel * 100) + "% ." + "Energy Saver Status: " + Battery.EnergySaverStatus;
-        Microsoft.Maui.Devices.EnergySaverStatus batterySaverStatus = Battery.EnergySaverStatus;
-        return batteryDetails;
+      
+
+       
+        return _cardInfo;
     }
-    // private  CardInfo TryScanCard()
-    // {
-        
-        // var scanResult =  _cardReader.ScanCard(true);
-        // var cardInfo = new CardInfo
-        // {
-        //     FaceImage = null,
-        //     BackImage = null,
-        //     FrontImage = null
-        //     
-        // };
-        // if (scanResult)
-        // {
-        //     // Xử lý hình ảnh mặt trước
-        //     var frontImage = _cardReader.GetScanImage()?.Front;
-        //     if (frontImage != null)
-        //     {
-        //         var jpgFront = frontImage.ToMemoryStream(".jpg").ToArray();
-        //         var scanFront = Convert.ToBase64String(jpgFront);
-        //         cardInfo.FrontImage = scanFront;
-        //     }
-        //
-        //     // Xử lý hình ảnh mặt sau
-        //     var backImage = _cardReader.GetScanImage()?.Back;
-        //     if (backImage != null)
-        //     {
-        //         var jpgBack = backImage.ToMemoryStream(".jpg").ToArray();
-        //         var scanBack = Convert.ToBase64String(jpgBack);
-        //         cardInfo.FrontImage = scanBack;
-        //         cardInfo.FaceImage = _cardReader.GetChipFaceImage();
-        //
-        //         // BackImageData = ConvertBase64ToImage(scanBack);
-        //     }
-        //     
-        //     
-        // }
-        //
-        // return cardInfo;
-    // }
+    private static void CardReader_OnStatusChanged(object sender, StatusChangeEventArgs e)
+    {
+        Console.WriteLine($"Device {e.ReaderName} change stated from: {e.LastState} to: {e.NewState}.");
+        if (e.LastState != SCRState.Empty || e.NewState != SCRState.Present)
+            return;
+
+        try
+        {
+         
+            var result = TryScanCard(e.ReaderName);
+            _cardInfo.BackImage = result.BackImage;
+            _cardInfo.FrontImage = result.FrontImage;
+            _cardInfo.FaceImage = result.FaceImage;
+
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error occured: " + ex.ToString());
+        }
+        finally
+        {
+            CardReader.RejectCard(); //Only support on SCAN-READER
+        }
+    }
+    [STAThread]
+    private static  CardInfo TryScanCard(string readerName)
+    {
+        try
+        {
+            var scanResult = CardReader.ScanCard(true);
+            var cardInfo = new CardInfo
+            {
+                FaceImage = null,
+                BackImage = null,
+                FrontImage = null
+
+            };
+            if (scanResult)
+            {
+                // Xử lý hình ảnh mặt trước
+                var frontImage = CardReader.GetScanImage()?.Front;
+                if (frontImage != null)
+                {
+                    var jpgFront = frontImage.ToMemoryStream(".jpg").ToArray();
+                    var scanFront = Convert.ToBase64String(jpgFront);
+                    cardInfo.FrontImage = scanFront;
+                }
+
+                // Xử lý hình ảnh mặt sau
+                var backImage = CardReader.GetScanImage()?.Back;
+                if (backImage != null)
+                {
+                    var jpgBack = backImage.ToMemoryStream(".jpg").ToArray();
+                    var scanBack = Convert.ToBase64String(jpgBack);
+                    cardInfo.BackImage = scanBack;
+
+
+
+                    // BackImageData = ConvertBase64ToImage(scanBack);
+                }
+
+                var canCode = CardReader.GetOcrCanCode();
+                var mrz = CardReader.GetChipMRZ();
+                CardReader.ReadCard(readerName, canCode, mrz);
+                cardInfo.FaceImage = CardReader.GetChipFaceImage();
+
+            }
+
+            return cardInfo;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            // CardReader.RejectCard();
+        }
+        finally
+        {
+            CardReader.RejectCard();
+        }
+
+        return new CardInfo();
+
+    }
 }
 
-// public class CardInfo
-// {
-//     public string? FaceImage { get; set; }
-//     public string? BackImage { get; set; }
-//     public string? FrontImage { get; set; }
-// }
+public class CardInfo
+{
+    public string? FaceImage { get; set; }
+    public string? BackImage { get; set; }
+    public string? FrontImage { get; set; }
+}
